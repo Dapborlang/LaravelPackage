9917fd324b16c6765b3d4a0032eb9c222d22c9c4
Dapbor <dapbor88@gmail.com>
2024-05-12 14:24:16 +0530

    added FormAPI

---
+++ Package/Rdmarwein/FormApi/src/js/formapi.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue';
+const app = createApp({});
+import ExampleComponent from './components/ExampleComponent.vue';
+app.component('example-component', ExampleComponent);
+app.mount('#app');
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import './bootstrap';
-import { createApp } from 'vue';
-const app = createApp({});
-import ExampleComponent from './components/ExampleComponent.vue';
-app.component('example-component', ExampleComponent);
-app.mount('#app');
+// import { createApp } from 'vue';
+// const app = createApp({});
+// import ExampleComponent from './components/ExampleComponent.vue';
+// app.component('example-component', ExampleComponent);
+// app.mount('#app');
